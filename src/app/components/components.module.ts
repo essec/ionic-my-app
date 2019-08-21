bdb6d0641c50756b1d2a5c6a09950b8ad949c50e
essec <jakee.indapanya@gmail.com>
2019-08-21 18:41:20 +0700

    init TimeAgoPipe

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,12 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TimeAgoPipe } from './time-ago.pipe';
 
 
 
 @NgModule({
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  declarations: [],
+  declarations: [TimeAgoPipe],
   imports: [
     CommonModule
   ]
