3ddfbe05b6e6636987f619b3128b6bfd2a63c6ae
JAKEE INDAPANYA (จาคี อินทปัญญา) <5910110044@email.psu.ac.th>
2019-10-11 16:00:12 +0700

    improve itmservice page174

--- src/app/services/services/item/item.service.ts
+++ src/app/services/services/item/item.service.ts
@@ -1,6 +1,6 @@
 
 import { Injectable } from '@angular/core';
-import { Observable, combineLatest } from 'rxjs';
+import { Observable, combineLatest, Subject } from 'rxjs';
 import * as isEqual from 'lodash.isequal';
 import { Items } from 'src/app/model/items';
 import { AngularFireDatabase } from '@angular/fire/database';
@@ -15,24 +15,32 @@ export interface Query {
 @Injectable({
   providedIn: 'root'
 })
-export class ItemService {
-  constructor(
-    private db: AngularFireDatabase
-  ) { }
 
-  load(offset: number, limit: number): Observable<Items> {
-    return this.db.list('/v0/topstories')
-      .valueChanges()
-      .pipe(
-        map(ids => ids.slice(offset, offset + limit)),
-        distinctUntilChanged(isEqual),
-        map((ids: any[]) => ids.map(id => this.db.object('/v0/item/' + id).valueChanges())),
-        map((items: any) => ({
-          offset,
-          limit,
-          total: limit,
-          results: items,
-        }))
-      );
+export class ItemService {
+  private queries: Subject<Query>;
+
+  constructor(private db: AngularFireDatabase) {
+    this.queries = new Subject<Query>();
   }
+
+  load(query: Query) {
+    this.queries.next(query);
+  }
+
+  // get(): Observable<Items>;
+  // load(offset: number, limit: number): Observable<Items> {
+  //   return this.db.list('/v0/topstories')
+  //     .valueChanges()
+  //     .pipe(
+  //       map(ids => ids.slice(offset, offset + limit)),
+  //       distinctUntilChanged(isEqual),
+  //       map((ids: any[]) => ids.map(id => this.db.object('/v0/item/' + id).valueChanges())),
+  //       map((items: any) => ({
+  //         offset,
+  //         limit,
+  //         total: limit,
+  //         results: items,
+  //       }))
+  //     );
+  // }
 }
