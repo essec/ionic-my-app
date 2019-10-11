c2703b09992cb8506e5369b98feaf0b8dc381fc6
JAKEE INDAPANYA (จาคี อินทปัญญา) <5910110044@email.psu.ac.th>
2019-10-11 14:31:30 +0700

    linting

--- src/app/model/item.ts
+++ src/app/model/item.ts
@@ -1,8 +1,8 @@
 export interface Item {
-    id: number
-    title: string
-    url: string
-    by: string
-    time: number
-    score: number
+    id: number;
+    title: string;
+    url: string;
+    by: string;
+    time: number;
+    score: number;
 }
