80eb1ff68083f92f1b539a248b3f82a127e1fc77
5910110044 <5910110044@R301-mini11.local>
2019-10-16 19:13:48 +0700

    updated item service

--- src/app/services/services/item/item.service.ts
+++ src/app/services/services/item/item.service.ts
@@ -31,45 +31,22 @@ export class ItemService {
   }
 
   get(): Observable<Items> {
-    this.rawItemIds = this.db.list<number>('/v0/topstories')
+    const rawItemIds = this.db.list<number>('/v0/topstories')
       .valueChanges();
-    this.rawItemIds.subscribe(ids => {
-      this.totalItem = ids.length;
-    });
-    // const loadings = this.queries.pipe(
-    //   filter(v => v.refresh),
-    //   map(query => ({
-    //     loading: true,
-    //     offset: query.offset,
-    //     limit: query.limit,
-    //     results: []
-    //   }))
-    // );
     const itemIds = combineLatest(
-      this.rawItemIds,
+      rawItemIds,
       this.queries
     ).pipe(
       filter(([ids, query]) => query.refresh),
       map(([ids, query]) => ids)
     );
-    const selector = ({ refresh = false, offset, limit }, ids) =>
+    const selector = ({ offset, limit }, ids) =>
       combineLatest(...(ids.slice(offset, offset + limit)
         .map(id => this.db.object<Item>('/v0/item/' + id).valueChanges()))
-      ).pipe(
-        map(items => ({
-          refresh,
-          total: this.totalItem,
-          results: items
-        }))
       ) as Observable<Items>;
-    
-    // return merge(
-    //   loadings, items
-    // )
     return merge(
       combineLatest(this.queries, itemIds).pipe(
-        map(([query, ids]) => selector(query, ids).
-        pipe(take(1)))
+        map(([query, ids]) => selector(query, ids).pipe(take(1)))
       ),
       this.queries.pipe(
         skip(1),
