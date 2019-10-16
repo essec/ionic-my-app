3ed58c98931dc92f3dd0cf142ab03c734636a917
5910110044 <5910110044@R301-mini11.local>
2019-10-16 19:13:22 +0700

    updated items model

--- src/app/model/items.ts
+++ src/app/model/items.ts
@@ -1,10 +1,12 @@
 import { Item } from './item';
 import { Observable } from 'rxjs';
+// import { type } from 'os';
 
-export interface Items {
-    offset: number;
-    limit: number;
-    total?: number;
-    results: Item[];
-    refresh?: boolean;
-}
+// export interface Items {
+//     offset: number;
+//     limit: number;
+//     total?: number;
+//     results: Item[];
+//     refresh?: boolean;
+// }
+export type Items = Item[];
